78bf3a5379a718637294a73a99ec3fd5b1280947
Ojas Patharkar <ojaspatharkar@Ojas.local>
2020-10-04 20:52:36 +0530

    test

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -121,6 +121,7 @@ function checkValidServiceWorker(swUrl, config) {
         registerValidSW(swUrl, config);
       }
     })
+    
     .catch(() => {
       console.log(
         'No internet connection found. App is running in offline mode.'
