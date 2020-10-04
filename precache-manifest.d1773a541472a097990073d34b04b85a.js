494911eeb0723746f690725230d1bd832588cf63
Ojas Patharkar <ojaspatharkar@Ojas.local>
2020-10-04 16:25:52 +0530

    Updates

---
+++ precache-manifest.d1773a541472a097990073d34b04b85a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c99775e381a91b61b7d1bb7924737be1",
+    "url": "/upstox/index.html"
+  },
+  {
+    "revision": "e7c9916bd1b640aba89c",
+    "url": "/upstox/static/css/main.b87fd5f9.chunk.css"
+  },
+  {
+    "revision": "875af7a2e69046a2199d",
+    "url": "/upstox/static/js/2.e99e7472.chunk.js"
+  },
+  {
+    "revision": "b85191dd510f8f121ca8c8c360144fda",
+    "url": "/upstox/static/js/2.e99e7472.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7c9916bd1b640aba89c",
+    "url": "/upstox/static/js/main.19b52a81.chunk.js"
+  },
+  {
+    "revision": "d3f24be24c7e39c4d845",
+    "url": "/upstox/static/js/runtime-main.3caa4f35.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/upstox/static/media/logo.5d5d9eef.svg"
+  }
+]);
